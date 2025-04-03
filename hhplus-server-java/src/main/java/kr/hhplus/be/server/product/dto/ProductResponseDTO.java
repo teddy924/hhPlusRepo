7dbe97baceaff83bd76ed8c395237303debc31e0
teddy924 <jongyong924@gmail.com>
2025-04-04 05:23:01 +0900

    서버 구축 - 요구사항 분석 및 설계 - swagger 작성 - product 도메인

---
+++ hhplus-server-java/src/main/java/kr/hhplus/be/server/product/dto/ProductResponseDTO.java
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.product.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "상품 응답 DTO")
+public class ProductResponseDTO {
+    private String productId;
+    private String productName;
+    private Long sellerId;
+    private String category;
+    private Long price;
+    private Integer stock;
+}
