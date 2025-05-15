18a26682f74f58051fc41ef0bf7d6197647a4963
teddy924 <jongyong924@gmail.com>
2025-05-16 03:41:54 +0900

    대용량 트래픽&데이터 처리 - 7주차 - 선착순 쿠폰 발급 기능 위치 변경

--- hhplus-server-java/src/main/java/kr/hhplus/be/server/application/coupon/CouponService.java
+++ hhplus-server-java/src/main/java/kr/hhplus/be/server/application/coupon/CouponService.java
@@ -1,13 +1,10 @@
 package kr.hhplus.be.server.application.coupon;
 
-import kr.hhplus.be.server.common.CacheKey;
 import kr.hhplus.be.server.common.exception.CustomException;
 import kr.hhplus.be.server.domain.coupon.*;
 import kr.hhplus.be.server.domain.coupon.entity.Coupon;
 import kr.hhplus.be.server.domain.coupon.entity.CouponIssue;
 import kr.hhplus.be.server.interfaces.coupon.CouponResponseDTO;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,14 +17,11 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
     private final CouponIssueRepository couponIssueRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
 
     public CouponService(CouponRepository couponRepository,
-                         CouponIssueRepository couponIssueRepository,
-                         @Qualifier("masterRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
+                         CouponIssueRepository couponIssueRepository) {
         this.couponRepository = couponRepository;
         this.couponIssueRepository = couponIssueRepository;
-        this.redisTemplate = redisTemplate;
     }
 
     // 보유 쿠폰 목록 조회
@@ -110,47 +104,5 @@ public class CouponService {
             throw new CustomException(FAIL_RESTORE_COUPON);
         }
     }
-
-    @Transactional
-    public void issueCouponFirstCome(CouponIssueCommand command) {
-        Long userId = command.userId();
-        Long couponId = command.couponId();
-
-        String stockKey = CacheKey.stock(couponId);             // 정렬을 위해 zset 사용
-        String issuedSetKey = CacheKey.issuedSet(couponId);     // 발급여부는 순서와 무관하므로 set 사용
-
-        // 중복 발급 여부 체크
-        Boolean alreadyIssued = redisTemplate.opsForSet().isMember(issuedSetKey, userId);       // 발급이력 set에 userId가 있는지 체크
-        if (Boolean.TRUE.equals(alreadyIssued)) {
-            throw new CustomException(DUPLICATE_ISSUE_COUPON);
-        }
-
-        // 발급 시도 (선착순 삽입)
-        long added = Boolean.TRUE.equals(redisTemplate.opsForZSet().add(stockKey, userId.toString(), System.currentTimeMillis())) ? 1L : 0L;        // 발급순서 zset에 유저를 현재 시각으로 add
-        if (added == 0L) {      // zset add에 실패하면 이미 해당 유저는 발급 시도 중
-            throw new CustomException(DUPLICATE_TRY_ISSUE);
-        }
-
-        // 발급 인원 초과 시 제거
-        Coupon coupon = couponRepository.getById(couponId);
-        int totalStock = coupon.getLimitQuantity();
-        Long rank = redisTemplate.opsForZSet().rank(stockKey, userId.toString());       // 유저가 몇 번째 발급자인지 추출
-        if (rank == null || rank >= totalStock) {       // 발급순서가 총 발급개능 개수와 일치하거나 크면 재고 없음
-            redisTemplate.opsForZSet().remove(stockKey, userId.toString());     // 재고가 없으면 발급 대기열 zset에서 해당 유저 제거
-            throw new CustomException(COUPON_SOLD_OUT);
-        }
-
-        // 쿠폰 수량 감소 (DB)
-        coupon.useOneQuantity();
-        couponRepository.save(coupon);
-
-        // 발급 이력 저장 (DB)
-        CouponIssue issue = CouponIssue.create(command.toInfo());
-        couponIssueRepository.save(issue);
-
-        // 발급 성공 기록
-        redisTemplate.opsForSet().add(issuedSetKey, userId.toString());     // 발급이력 set에 유저 add
-    }
-
 
 }
