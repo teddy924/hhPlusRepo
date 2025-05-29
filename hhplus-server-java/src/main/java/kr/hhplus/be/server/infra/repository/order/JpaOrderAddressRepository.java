4923406a9a0c9727732a6ad8c720aa310c8d8024
teddy924 <jongyong924@gmail.com>
2025-05-29 13:39:55 +0900

    대용량 트래픽&데이터 처리 - 9주차 - repository 패키지 정리

---
+++ hhplus-server-java/src/main/java/kr/hhplus/be/server/infra/repository/order/JpaOrderAddressRepository.java
@@ -0,0 +1,14 @@
+package kr.hhplus.be.server.infra.repository.order;
+
+import kr.hhplus.be.server.domain.order.entity.OrderAddress;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaOrderAddressRepository extends JpaRepository<OrderAddress, Long> {
+
+    @NonNull
+    Optional<OrderAddress> findByOrderId(@NonNull Long orderId);
+
+}
---
+++ hhplus-server-java/src/main/java/kr/hhplus/be/server/infra/repository/order/OrderAddressRepositoryImpl.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.infra.repository.order;
+
+import kr.hhplus.be.server.common.exception.CustomException;
+import kr.hhplus.be.server.domain.order.OrderAddressRepository;
+import kr.hhplus.be.server.domain.order.entity.OrderAddress;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static kr.hhplus.be.server.config.swagger.ErrorCode.*;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderAddressRepositoryImpl implements OrderAddressRepository {
+
+    private final JpaOrderAddressRepository jpaOrderAddressRepository;
+
+    @Override
+    public OrderAddress getByOrderId(Long orderId) {
+        return jpaOrderAddressRepository.findByOrderId(orderId).orElseThrow(() -> new CustomException(NOT_EXIST_ORDER_ADDRESS));
+    }
+
+    @Override
+    public void save(OrderAddress orderAddress) {
+        jpaOrderAddressRepository.save(orderAddress);
+    }
+
+}
